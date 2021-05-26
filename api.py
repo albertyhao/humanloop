c2257d9aa978bada4b3a14564c4562ffaea3625e
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2021-05-25 20:28:59 -0700

    api edits

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/save', methods=["POST"])
+def save():
+    print(request.get_json())
+    print(request.query_string.decode()) #parse
+    return jsonify(success=True), 200
